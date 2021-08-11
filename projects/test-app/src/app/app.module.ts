fe7445fc190d92274c6a9be05eb2e8ee56593135
Benjamin Kindle <Benjamin.kindle@hyland.com>
2021-08-11 09:22:20 -0400

    create app, library, and jest tests

---
+++ projects/test-app/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { GridsterModule } from "angular-gridster2";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, GridsterModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
